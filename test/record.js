d6f3fdc58c791e91260f472b199ea80f55e1414c
Jordan Duggan <Jordan.Duggan@Gmail.com>
2015-02-16 12:43:32 -0500

    Added nock recorder to create fixtures and remove old fixtures

---
+++ test/record.js
@@ -0,0 +1,52 @@
+var nock = require('nock');
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function (name, options) {
+
+  // Options tell us where to store our fixtures
+  options = options || {};
+
+  // Default options
+  var test_folder = options.test_folder || 'test';
+  var fixtures_folder = options.fixtures_folder || 'fixtures';
+  var fp = path.join(test_folder, fixtures_folder, name + '.js');
+
+  /*
+  `has_fixtures` indicates whether the test has fixtures we should read,
+  or doesn't, so we should record and save them.
+  the environment variable `NOCK_RECORD` can be used to force a new recording.
+  */
+  var has_fixtures = !!process.env.NOCK_RECORD;
+
+  return {
+
+    // Use existing fixture, or start recording to create a new one
+    before: function () {
+      if (!has_fixtures) try {
+        require('../' + fp);
+        has_fixtures = true;
+      } catch (e) {
+        nock.recorder.rec({
+          dont_print: true
+        });
+      } else {
+        has_fixtures = false;
+        nock.recorder.rec({
+          dont_print: true
+        });
+      }
+    },
+
+    // If a recording was created, save it as a new fixuture
+    after: function (done) {
+      if (!has_fixtures) {
+        has_fixtures = nock.recorder.play();
+        var text = "var nock = require('nock');\n" + fixtures.join('\n');
+        fs.writeFile(fp, text, done);
+      } else {
+        done();
+      }
+    }
+  }
+};
